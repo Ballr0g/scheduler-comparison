b5fa33da5e449e75e4fd53893e7931788dd8ba5d
Ballr0g <danielsagel@outlook.com>
2024-11-08 22:25:57 +0300

    [MP-006] Initial JobRunr implementation setup - added necessary dependencies, organized application.yaml and added Liquibase tables.

--- scheduler-jobrunr/build.gradle.kts
+++ scheduler-jobrunr/build.gradle.kts
@@ -8,6 +8,11 @@ plugins {
 group = "io.scheduler.comparison.jobrunr"
 version = "0.0.1-SNAPSHOT"
 
+val liquibaseVersion: String by project
+val postgresVersion: String by project
+val kotlinLoggingVersion: String by project
+val jobRunrVersion: String by project
+
 java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
@@ -19,11 +24,32 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+    // --> Implementation Dependencies <--
+    // Spring Boot starter JDBC.
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    // Spring Boot JobRunr starter
+    implementation("org.jobrunr:jobrunr-spring-boot-3-starter:${jobRunrVersion}")
+    // Spring Kafka.
+    implementation("org.springframework.kafka:spring-kafka")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // Liquibase.
+    implementation("org.liquibase:liquibase-core:${liquibaseVersion}")
+    // PostgreSQL.
+    implementation("org.postgresql:postgresql:${postgresVersion}")
+    // Kotlin logging.
+    implementation("io.github.oshai:kotlin-logging:${kotlinLoggingVersion}")
+    implementation("org.hibernate.validator:hibernate-validator")
+
+
+    // --> Test Implementation Dependencies <--
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
+    // --> Test Runtime-Only Dependencies <--
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // --> Development-Only Dependencies <--
+    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 }
 
 kotlin {
