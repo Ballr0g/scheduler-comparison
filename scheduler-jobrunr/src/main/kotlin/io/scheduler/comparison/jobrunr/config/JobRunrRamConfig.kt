f0c44bd5248b118a2d6f52456a2210e0398c1c4b
Ballr0g <danielsagel@outlook.com>
2024-11-19 22:34:19 +0300

    [MP-007] Added a config specific to the ram profile.

---
+++ scheduler-jobrunr/src/main/kotlin/io/scheduler/comparison/jobrunr/config/JobRunrRamConfig.kt
@@ -0,0 +1,23 @@
+package io.scheduler.comparison.jobrunr.config
+
+import org.jobrunr.jobs.mappers.JobMapper
+import org.jobrunr.storage.InMemoryStorageProvider
+import org.jobrunr.storage.StorageProvider
+import org.jobrunr.utils.mapper.jackson.JacksonJsonMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+
+
+@Configuration
+@Profile("ram")
+class JobRunrRamConfig {
+
+    @Bean
+    fun storageProvider(jobMapper: JobMapper): StorageProvider {
+        val storageProvider = InMemoryStorageProvider()
+        storageProvider.setJobMapper(JobMapper(JacksonJsonMapper()))
+        return storageProvider
+    }
+
+}
