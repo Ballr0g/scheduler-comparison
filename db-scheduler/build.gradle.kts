9b46defe3de882d2e3b85e5f0fadd70eac973e71
Ballr0g <danielsagel@outlook.com>
2024-12-11 21:34:08 +0300

    [MP-010] Added base dependencies for db-scheduler.

--- db-scheduler/build.gradle.kts
+++ db-scheduler/build.gradle.kts
@@ -8,6 +8,12 @@ plugins {
 group = "io.scheduler.comparison.dbscheduler"
 version = "0.0.1-SNAPSHOT"
 
+val liquibaseVersion: String by project
+val postgresVersion: String by project
+val kotlinLoggingVersion: String by project
+val dbSchedulerVersion: String by project
+val arrowKtVersion: String by project
+
 java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(21)
@@ -19,11 +25,37 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	// --> Implementation Dependencies <--
+	// Spring Boot starter JDBC
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	// db-scheduler Spring Boot starter
+	implementation("com.github.kagkarlsson:db-scheduler-spring-boot-starter:${dbSchedulerVersion}")
+	// Spring Kafka.
+	implementation("org.springframework.kafka:spring-kafka")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	// Liquibase.
+	implementation("org.liquibase:liquibase-core:${liquibaseVersion}")
+	// PostgreSQL.
+	implementation("org.postgresql:postgresql:${postgresVersion}")
+	// Kotlin logging
+	implementation("io.github.oshai:kotlin-logging:${kotlinLoggingVersion}")
+	implementation("org.hibernate.validator:hibernate-validator")
+	// Jackson
+	implementation("com.fasterxml.jackson.core:jackson-databind")
+	implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+	// Arrow (Either)
+	implementation("io.arrow-kt:arrow-core:${arrowKtVersion}")
+
+
+	// --> Test Implementation Dependencies <--
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+
+	// --> Test Runtime-Only Dependencies <--
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// --> Development-Only Dependencies <--
+	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 }
 
 kotlin {
